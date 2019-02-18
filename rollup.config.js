30eee12bb084bee0d76291c76126019f017a0625
Jonathas Carrijo <jonathascarrijo@gmail.com>
2019-02-18 23:22:03 +0000

    Add MaskService

--- rollup.config.js
+++ rollup.config.js
@@ -9,7 +9,7 @@ let includePathOptions = {
 };
 
 
-export default {
+export default [{
   input: "src/text-input-mask.js",
   output: {
     file: "dist/text-input-mask.js",
@@ -17,15 +17,24 @@ export default {
   },
   name: 'ReactTextMask',
   globals: {
-    react: 'React'
-  },
-  external: [ 'react' ],
-  globals: {
     react: 'react'
   },
+  external: [ 'react' ],
   plugins: [
     copy({'src/masks/internal-dependencies': 'dist/internal-dependencies'}),
     resolve({jsnext: true}),
     babel()
   ]
-}
+},{
+  input: 'src/mask-service.js',
+  output: {
+    file: 'dist/mask-service.js',
+    format: 'umd'
+  },
+  name: "MaskService",
+  plugins: [
+    copy({'src/masks/internal-dependencies': 'dist/internal-dependencies'}),
+    resolve({jsnext: true}),
+    babel()
+  ]
+}];
