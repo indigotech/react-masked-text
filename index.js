272608367b838760ea54592a32960020d3e78df2
Jonathas Carrijo <jonathascarrijo@gmail.com>
2019-02-18 21:14:05 +0000

    Export MaskService

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 import TextInputMask from './dist/text-input-mask';
+import MaskService from './src/mask-service';
 
 module.exports.TextInputMask = TextInputMask;
+module.exports.MaskService = MaskService;
