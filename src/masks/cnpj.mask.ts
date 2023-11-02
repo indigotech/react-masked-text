83bd22c97e07fab43fd2bf32eccb06ebd59e46e5
Lucas Leonardo <caslujpg@gmail.com>
2023-11-02 13:49:46 -0300

    bump: 1.0.2

--- src/masks/cnpj.mask.ts
+++ src/masks/cnpj.mask.ts
@@ -6,20 +6,19 @@ const validateCnpj = (cnpj: string): boolean => {
   const valida: number[] = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
   let dig1 = 0;
   let dig2 = 0;
-  let i = 0;
 
   const exp = /\.|\-|\//g;
   cnpj = cnpj.toString().replace(exp, '');
-  const digito = Number(eval(cnpj.charAt(12) + cnpj.charAt(13)));
+  const digito = Number(cnpj.charAt(12) + cnpj.charAt(13));
 
-  for (i = 0; i < valida.length; i++) {
+  for (let i = 0; i < valida.length; i++) {
     dig1 += i > 0 ? Number(cnpj.charAt(i - 1)) * valida[i] : 0;
     dig2 += Number(cnpj.charAt(i)) * valida[i];
   }
   dig1 = dig1 % 11 < 2 ? 0 : 11 - (dig1 % 11);
   dig2 = dig2 % 11 < 2 ? 0 : 11 - (dig2 % 11);
 
-  return dig1 * 10 + dig2 == digito;
+  return dig1 * 10 + dig2 === digito;
 };
 
 class CnpjMask extends BaseMask {
